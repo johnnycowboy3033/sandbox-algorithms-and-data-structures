32cdcefab79289db1d2d2d498b739ec17dfa9822
Pope Isaac Joule VII <112647925+johnnycowboy3033@users.noreply.github.com>
2024-04-19 20:19:51 -0400

    commit

---
+++ CodeBase/Libraries/Customs/Components/js/spec/Data.Spec.js
@@ -0,0 +1,13 @@
+describe("Binary Search Algorithm", function() {
+
+    it("(JASMINE SETUP) Should be true because this test method is for testing the Jasmine setup", function() {
+
+        console.log("=== JASMINE SETUP ===");
+        expect(true).toBeTruthy();
+
+    });
+    
+ 
+
+
+  });
